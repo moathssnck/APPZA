5bb1548048ccd5071ec9b308520ddcb24378f959
moathssnck <myneww00@gmail.com>
2025-06-30 15:12:43 +0300

    ssss

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Quick App',
-  description: 'Quick with App',
+  title: 'General Services App',
+  description: 'General Telecommunicate Services App',
 }
 
 export default function RootLayout({
